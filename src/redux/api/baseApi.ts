cad22a74c04983b7dfebe532f51ec6a0bbbcce81
NiharMondal <developernihar@gmail.com>
2024-07-13 00:38:31 +0600

    add: credentials

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -7,7 +7,9 @@ const BASE_URL = "https://portfolio-dashboard-backend-plum.vercel.app/api/v1";
 
 export const baseApi = createApi({
 	reducerPath: "baseApi",
-	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL,
+	baseQuery: fetchBaseQuery({ 
+		baseUrl: BASE_URL,
+		credentials:"include",
 		prepareHeaders: (headers) => {
 			// By default, if we have a token in the store, let's use that for authenticated requests
 			const token = tokenHelper.getToken(tokenName)
